19d218d77551e30d38e5110618934f21bd14fc36
Eve Barabas <b_seven_e@yahoo.co.uk>
2013-09-10 09:54:17 +0100

    Solution to roman numerals done via mob programming

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -4,9 +4,30 @@
 class RomanNumerals
   def initialize(number)
     @number = number
+    @cache = {}
   end
   
   def arabic_to_roman
-    true
+    @cache[@number] ||= "".tap do |result|
+      current = @number
+      {
+        'M'  => 1000,
+        'CM' => 900,
+        'C'  => 100,
+        'XC' => 90,
+        'L'  => 50,
+        'XL' => 40,
+        'X'  => 10,
+        'IX' => 9,
+        'V'  => 5,
+        'IV' => 4,
+        'I'  => 1,
+      }.each do |roman,arabic|
+        while current >= arabic do
+          result << roman
+          current -= arabic
+        end
+      end
+    end
   end
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -1,11 +1,31 @@
 require_relative '../lib/roman_numerals'
 
-describe "Roman Numerals" do
+describe RomanNumerals do
   def arabic_to_roman_rep(number)
     RomanNumerals.new(number).arabic_to_roman
   end
   
-  specify 'true should be true' do
-    arabic_to_roman_rep("blah").should be_true
+  describe "#arabic_to_roman" do
+    {
+      1 => ?I,
+      2 => 'II',
+      3 => 'III',
+      4 => 'IV',
+      5 => 'V',
+      6 => 'VI',
+      7 => 'VII',
+      8 => 'VIII',
+      9 => 'IX',
+      10 => 'X',
+      20 => 'XX',
+      1990 => 'MCMXC',
+      2008 => 'MMVIII',
+      99 => 'XCIX',
+      47 => 'XLVII'
+    }.each do |arabic,roman|
+      specify "the number #{arabic} should be #{roman}" do
+        expect(arabic_to_roman_rep(arabic)).to eq(roman)
+      end
+    end
   end
 end
